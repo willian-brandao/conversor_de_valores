bc7842d6ac0acde1a353a938abb75ca3fd0b6002
Willian Brandão Ramos <willianramos.wrk@gmail.com>
2023-10-14 13:36:20 -0300

    Modificado a variavel a para a variavel lista para ficar mais clara a função da variavel. Foi documenta a função de conversão para numeros binários

--- binario.py
+++ binario.py
@@ -7,7 +7,7 @@ def decimal_para_binario(numero):
 #criando uma lista vazia que irá armazenar os resultados de resto 0 ou 1 das divisões feitas
     num = []
 #criando uma string vazia que vai receber a string
-    a = ''
+    lista = ''
 #criando uma contador para inclementar o laço de repetição 
     cont = 0
 #laço de repetição onde será feita a divisão do número recebido até ele chegar a 0 ou 1
@@ -20,7 +20,7 @@ def decimal_para_binario(numero):
 #este laço de repetição irá percorrer a lista e converter em uma string
 #obs: os valores serão percorridos em ordem decrescente
     for i in range( cont -1, -1, -1):
-        a += str(num[i])
-    return a
+        lista += str(num[i])
+    return lista
 
 main()
